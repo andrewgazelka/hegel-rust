7ea9a84fd524309397683abaf363d345af2ff076
Michael Gibson <michael.gibson@antithesis.com>
2026-02-09 15:37:45 -0500

    Updating flake.nix and creating default.nix

--- flake.nix
+++ flake.nix
@@ -4,16 +4,31 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     hegel.url = "git+ssh://git@github.com/antithesishq/hegel";
+    flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
   };
 
   outputs =
-    { nixpkgs, hegel, ... }:
+    { 
+      nixpkgs, 
+      hegel, 
+      ... 
+    }:
     let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      packages.${system}.default = pkgs.rustPlatform.buildRustPackage {
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+    mkHegelRustProject = 
+      {
+        pkgs,
+        system ? pkgs.system,
+      }@args: let
+      in pkgs.rustPlatform.buildRustPackage {
         pname = "hegel";
         version = "0.1.0";
         src = ./.;
@@ -21,15 +36,40 @@
 
         # hegel binary on PATH so build.rs finds it
         nativeBuildInputs = [ hegel.packages.${system}.default ];
+      };
+    in
+    {
 
+      # Export the builder for users
+      lib = {
+        inherit mkHegelRustProject;
       };
 
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = [
-          pkgs.cargo
-          pkgs.rustc
-          hegel.packages.${system}.default
-        ];
-      };
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          lib = pkgs.lib;
+        in
+        {
+          default = mkHegelRustProject { inherit pkgs; };
+        }
+      );
+
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = [
+              pkgs.cargo
+              pkgs.rustc
+              hegel.packages.${system}.default
+            ];
+          };
+        }
+      );
     };
 }
